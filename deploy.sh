01ad8eef7c28f789d9ab3728eaebcf902d7e031b
Andrey Sidorov <andrey.sidorov@flant.com>
2017-09-12 13:18:50 +0300

    replace clickhouse with mongo

--- deploy.sh
+++ deploy.sh
@@ -10,8 +10,10 @@ kctl() {
     kubectl --namespace "$NAMESPACE" "$@"
 }
 
-echo "Deploying Clickhouse"
-kctl apply -f manifests/clickhouse/clickhouse.yaml
+echo "Deploying MoongoDB"
+kctl apply -f manifests/mongo/mongodb.yaml
+#echo "Deploying Clickhouse"
+#kctl apply -f manifests/clickhouse/clickhouse.yaml
 #echo "Init DB in Clickhouse"
 #kctl apply -f manifests/clickhouse/init-job.yaml
 #sleep 10
