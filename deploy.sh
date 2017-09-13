7fd21cf9d35203c71068e5b2e49f62d1cdf58500
Andrey Sidorov <andrey.sidorov@flant.com>
2017-09-13 08:25:33 +0300

    percona mongodb

--- deploy.sh
+++ deploy.sh
@@ -10,10 +10,12 @@ kctl() {
     kubectl --namespace "$NAMESPACE" "$@"
 }
 
-echo "Deploying PostgreSQL"
-kctl apply -f manifests/postgres/postgresql.yaml
+#echo "Deploying PostgreSQL"
+#kctl apply -f manifests/postgres/postgresql.yaml
 #echo "Deploying MoongoDB"
 #kctl apply -f manifests/mongo/mongodb.yaml
+echo "Deploying Percona MongoDB"
+kctl apply -f manifests/mongo/percona-mongodb.yaml
 #echo "Deploying Clickhouse"
 #kctl apply -f manifests/clickhouse/clickhouse.yaml
 #echo "Init DB in Clickhouse"
--- dockerfiles/fluentd/insert_ch.sh
+++ dockerfiles/fluentd/insert_ch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat $1 | clickhouse-client --host="${CLICKHOUSE_HOST}" --port="${CLICKHOUSE_PORT}" --user="${CLICKHOUSE_USER}" --database="${CLICKHOUSE_DB}"  --query="INSERT INTO ${K8S_LOG_TABLE} FORMAT JSONEachRow";
+cat $1 | clickhouse-client --host="${CLICKHOUSE_HOST}" --port="${CLICKHOUSE_PORT}" --user="${CLICKHOUSE_USER}" --database="${CLICKHOUSE_DB}"  --query="INSERT INTO ${K8S_LOG_TABLE} FORMAT JSONEachRow"; && rm -f $1
