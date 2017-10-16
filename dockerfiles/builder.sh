2d1ce130313a5de7d4ce3a257c27e453a6c69f8b
Andrey Sidorov <andrey.sidorov@flant.com>
2017-10-16 17:36:58 +0300

    fix

---
+++ dockerfiles/builder.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Building clickhouse server..."
+cd clickhouse/ && \
+  docker build -t clickhouse . && \
+  docker tag clickhouse wimbo/clickhouse && \
+  docker push wimbo/clickhouse && \
+  cd .. && \
+echo "Building clickhouse server complete!"
+
+echo "Building fluentd..."
+cd fluentd/ && \
+  docker build -t fluentd . && \
+  docker tag fluentd wimbo/fluentd && \
+  docker push wimbo/fluentd && \
+  cd .. && \
+echo "Building fluentd complete!"
+
+echo "Building tabix..."
+cd tabix.ui/ && \
+  docker build -t tabix . && \
+  docker tag tabix wimbo/tabix && \
+  docker push wimbo/tabix && \
+  cd .. && \
+echo "Building tabxi complete!" 
